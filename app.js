0b6b01020b0f25a04a3957f3f64b84663a81c30a
Curtis Patterson <Dpatter94@yahoo.com>
2024-04-19 12:12:02 -0400

    Creating repo with template index.html and app.js and connecting them

---
+++ app.js
@@ -0,0 +1,9 @@
+// Global Variables
+
+const flavors = ["vanilla", "strawberry", "coffe"];
+
+// Functions
+
+// Actions being called and what functions are ebing called
+
+// Functions being called
